8271ce85dd10b44806700b7b8367ea2670823869
Steven Greulich <greulich.steven@gmail.com>
2018-04-09 18:20:18 -0700

    commit - Team A Score
    The purpose of this commit is that the Team A score has been setup

--- app/src/main/java/com/example/greul/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/greul/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.greul.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    int teamAScore = 0;
+
+    public void addThreePointsA(View view){
+        teamAScore += 3;
+        displayForTeamA(teamAScore);
+    }
+
+    public void addTwoPointsA(View view){
+        teamAScore += 2;
+        displayForTeamA(teamAScore);
+    }
+
+    public void addFreeThrowA(View view){
+        teamAScore += 1;
+        displayForTeamA(teamAScore);
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
